38b95b519f99822849919a8b1528f2647178d8e6
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-04-02 08:38:33 -0700

    fix: Show entire Processor ID

--- src/Chapter18/Listing18.21.LimitingAnAttributesUsageWithAttributeUsageAttribute.cs
+++ src/Chapter18/Listing18.21.LimitingAnAttributesUsageWithAttributeUsageAttribute.cs
@@ -1,6 +1,6 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_21;
 
-#region INCLUDE
+0#region INCLUDE
 // Restrict the attribute to properties and methods
 #region HIGHLIGHT
 [AttributeUsage(
